8e493a541b460a415adf627131198d3aa146bd17
백민정 <ggamji09@naver.com>
2026-03-22 15:12:57 +0900

    HealthController 추가, Swagger Parameter ì¶추가

--- src/main/java/com/card/payment/billing/BillingApplication.java
+++ src/main/java/com/card/payment/billing/BillingApplication.java
@@ -9,8 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
  * 카드 매입 청구 서비스 애플리케이션
  */
 @SpringBootApplication
-@EnableJpaAuditing
-@EnableFeignClients
+@EnableJpaAuditing  // createdAt 자동 저장
+@EnableFeignClients  // OpenFeign 활성화
 public class BillingApplication {
     public static void main(String[] args) {
         SpringApplication.run(BillingApplication.class, args);
--- src/main/java/com/card/payment/billing/client/AuthorizationClient.java
+++ src/main/java/com/card/payment/billing/client/AuthorizationClient.java
@@ -7,9 +7,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
+/**
+ * OpenFeign이 이 인터페이스를 보고 자동으로 HTTP 클라이언트(구현체)를 만들어줌
+ *
+ * name = "wooricard-approval-service"
+ *   → Eureka에 등록된 서비스 이름
+ *   → URL 하드코딩 없이
+ *     Eureka가 자동으로 주소 찾아줌
+ *
+ */
 @FeignClient(name = "wooricard-approval-service")
 public interface AuthorizationClient {
 
+    // GET http://유림IP:8081/api/authorization/approved/monthly
     @GetMapping("/api/authorization/approved/monthly")
     List<AuthorizationHistoryResponse> getMonthlyApproved(
             @RequestParam String cardNumberMasked,
